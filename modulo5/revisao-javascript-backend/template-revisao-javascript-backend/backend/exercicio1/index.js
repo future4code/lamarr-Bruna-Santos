b4dd6d7c940e3bc822c5344f591ccfcf2459e2cb
Bruna de Carvalho <brunacarvalho260697@gmail.com>
2022-10-05 00:08:39 -0300

    Finalização do primeiro exercicio: Temperaturas

--- modulo5/revisao-javascript-backend/template-revisao-javascript-backend/backend/exercicio1/index.js
+++ modulo5/revisao-javascript-backend/template-revisao-javascript-backend/backend/exercicio1/index.js
@@ -1,23 +1,28 @@
-console.log("exercicio 4");
+//Conversor de temperatura:
+//Crie um conversor de temperaturas para as escalas em Fahrenheit e Kelvin. 
 
-//“Declare uma variável e atribua a ela um objeto que representa um cliente de um banco.
-// Inicialize o objeto com pelo menos três propriedades e um método.”
-const cliente = {
-        nome:"Bruna",
-        idade:"25",
-        profissao:"Dev",
-        login: () => {
-            console.log("Usuário logado com sucesso") 
-        }
+//Entrada
+//O valor numérico em grau Celsius e uma string. “F” para Fahrenheit e “K” para Kelvin.
+
+//Saída
+//Saída: uma frase correspondente.
+
+//Validações
+//Depois de finalizar a implementação básica, trate os valores de entrada. O primeiro 
+//parâmetro deve ser sempre um número e o segundo uma string “F” ou “K” maiúsculas.
+
+const validacao = (texto) =>{
+    console.log(texto)
+}
+
+function temperatura (texto, num1, callback) {
+    if(texto === "K"){
+        callback(` ${num1}° Celsius é equivalente a ${num1+273.15}° Kelvin`)
+    }else if (texto === "F"){
+        callback(`  ${num1}° Celsius é equivalente a ${(1.8 * num1)+32}° Farenheit`)
+    }else if (texto !== "F" || texto !== "K"){
+        callback(`Erro. Parâmentro ${texto} inválido`)
     }
-    cliente.login()
+}
 
-    const nome = cliente.nome 
-    console.log(nome)
-
-/*     //Crie uma função que soma dois números e utiliza um callback de validação para tratar erros de entrada.
-function soma (num1, num2) {
-    return(
-        2 + 3
-    )
-} */
+temperatura("A", 40, validacao)
