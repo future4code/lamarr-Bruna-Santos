476465fde21c4e6cf023dda10c511fcc96f71ba4
Bruna de Carvalho <brunacarvalho260697@gmail.com>
2022-10-05 20:15:35 -0300

    FEAT: Exercicio sistema de logins, validação da senha funcionando

--- modulo5/revisao-javascript-backend/template-revisao-javascript-backend/backend/exercicio3/index.js
+++ modulo5/revisao-javascript-backend/template-revisao-javascript-backend/backend/exercicio3/index.js
@@ -1,1 +1,23 @@
-console.log("exercicio 3");
+// SISTEMA DE LOGIN
+
+const contas = [
+	{
+		email: "astrodev@labenu.com",
+		password: "abc123"
+	},
+	{
+		email: "bananinha@gmail.com",
+		password: "bananinha"
+	}
+]
+
+function cadastrarContas(cliente){
+    if(cliente.password.length >= 6){
+        contas.push(cliente)        
+    }else{
+        console.log("Sua senha deve possuir no mínimo 6 caracteres")
+    }
+}
+
+cadastrarContas({email: "teste@gmail.com", password:"aaaaa"})
+console.log(contas)
