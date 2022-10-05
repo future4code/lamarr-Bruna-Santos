ca702c4b5d14030f37c5fce7808da8a1e991d16f
Bruna de Carvalho <brunacarvalho260697@gmail.com>
2022-10-05 20:59:40 -0300

    FEAT: Resolução do exercicio 4

--- modulo5/revisao-javascript-backend/template-revisao-javascript-backend/backend/exercicio4/exercicio.js
+++ modulo5/revisao-javascript-backend/template-revisao-javascript-backend/backend/exercicio4/exercicio.js
@@ -1,1 +1,32 @@
-console.log("exercicio 4");
+// Gerador de itens únicos
+// A ideia é unir duas listas de objetos, mas sem repetir os que possuem 
+//o mesmo valor em uma propriedade.
+
+const primeiraLista = [
+	{
+		nome: "Banana"
+	},
+	{
+		nome: "Laranja"
+	}
+]
+
+const segundaLista= [
+	{
+		nome: "Laranja"
+	},
+	{
+		nome: "Cebola"
+	}
+]
+
+const terceiraLista = [...primeiraLista,...segundaLista]
+
+
+const listaSemRepeticao = (array) => [
+    ...terceiraLista.reduce(
+        (map, item)=> map.set(item.nome, item), new Map()
+    ).values()
+]
+
+console.log(listaSemRepeticao(terceiraLista))
