dd957dfd4011a89f57d626e29bc22a4498bb0a6d
Bruna de Carvalho <brunacarvalho260697@gmail.com>
2022-10-05 18:28:19 -0300

    Melhoria no exercicio 2: Criação de uma function para fazer o push e validação para não ter o id repetido

--- modulo5/revisao-javascript-backend/template-revisao-javascript-backend/backend/exercicio2/index.js
+++ modulo5/revisao-javascript-backend/template-revisao-javascript-backend/backend/exercicio2/index.js
@@ -8,15 +8,42 @@ const clientes = [
 	{ id: 4, nome: "Fulana" }
 ]
 
-console.log(clientes)
-
+/* console.log(clientes)
+ */
 // Inserindo de 1 em 1:
 clientes.push({id: 5, nome:"Bruna"})
 clientes.push({id: 6, nome:"José"})
 
 // Inserindo 2 de uma vez só:
 clientes.push({id: 7, nome:"Maria"}, {id: 8, nome:"Claudinha"})
+/* console.log(clientes)
+ */
+//CRIANDO UMA FUNCTION PARA ISSO: 
+
+function cadastraClientes (cliente){
+    clientes.push(cliente)
+}
+
+cadastraClientes({id: 9, nome:"Raphael"})
+/* console.log(clientes)
+ */
+// VALIDAÇÕES:
+
+function cadastraClientesValidacao (cliente){
+    const validacao=clientes.find((clientesExistentes)=>{
+       return clientesExistentes.id === cliente.id
+    })
+    if(validacao){
+        console.log("Erro. Parâmetro inválido (id já existe)")
+    }else{
+        clientes.push(cliente)
+    }
+}
+
+cadastraClientesValidacao({id: 11, nome:"Joelma"})
 console.log(clientes)
+
+
 
 
 
