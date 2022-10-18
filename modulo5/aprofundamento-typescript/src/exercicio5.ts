746aab1e6713806cecd2623e8c35a09be2d746c6
Bruna de Carvalho <brunacarvalho260697@gmail.com>
2022-10-18 13:56:22 -0300

    finalizaçao do exercicio 5

---
+++ modulo5/aprofundamento-typescript/src/exercicio5.ts
@@ -0,0 +1,18 @@
+type consultas = {
+    nome:string,
+    idade:number,
+    dataDaConsulta:string
+}
+
+const pacientes:consultas[] = [
+    { nome: "João", idade: 23, dataDaConsulta: "01/10/2021" },
+    { nome: "Pedro", idade: 31, dataDaConsulta: "02/07/2021" },
+    { nome: "Paula", idade: 26, dataDaConsulta: "03/11/2021" },
+    { nome: "Márcia", idade: 45, dataDaConsulta: "04/05/2021" }
+  ]
+  
+pacientes.sort(function(a,b){
+    return a.nome < b.nome? -1 : a.nome > b.nome ? 1:0
+})
+
+console.table(pacientes)
