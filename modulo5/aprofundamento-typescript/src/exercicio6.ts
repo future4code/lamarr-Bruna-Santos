dbed208a59496400971b65d38f1f152c8055b0a7
Bruna de Carvalho <brunacarvalho260697@gmail.com>
2022-10-18 17:06:33 -0300

    Exercicio 6: Lógica do desonto funcionando, falta apenas o array

---
+++ modulo5/aprofundamento-typescript/src/exercicio6.ts
@@ -0,0 +1,28 @@
+type produto ={
+    nome:string,
+    preco:number,
+    classificacao:string
+}
+
+const cadastrarProduto: produto[] =[
+    {
+        nome:"Blusa",
+        preco:40,
+        classificacao:"inverno"
+    }
+]
+
+
+function procurarProduto(nome:string, preco:number, classificacao:string): number{
+    if(classificacao === "verao"){
+        return (preco - (preco * 5)/100)
+    }else if (classificacao === "inverno"){
+        return (preco - (preco * 10)/100)
+    }else if (classificacao === "banho"){
+        return (preco - (preco*4)/100)
+    }else if (classificacao === "intimas"){
+        return (preco - (preco*7)/100)
+    }
+}
+
+console.log(procurarProduto("Camiseta", 40, "banho"))
