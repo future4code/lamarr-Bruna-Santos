d486219972bce734ace790fbdccde9a92e1150cb
Bruna de Carvalho <brunacarvalho260697@gmail.com>
2022-10-18 22:05:19 -0300

    FIX: Finalização do exercicio 6, agora está aparecendo o array junto com o preço com desconto

--- modulo5/aprofundamento-typescript/src/exercicio6.ts
+++ modulo5/aprofundamento-typescript/src/exercicio6.ts
@@ -4,21 +4,47 @@ type produto ={
     classificacao:string
 }
 
-function procurarProduto(nome:string, preco:number, classificacao:string): number{
+function procurarProduto(produtos:produto[]) :any{
+    return produtos.map((item:produto)=>{
+        if(item.classificacao === "verao"){
+            return {
+                nome: item.nome,
+                preco: item.preco,
+                classificacao: item.classificacao,
+                precoDesconto:(item.preco - (item.preco * 5)/100)
+            } 
+        }else if (item.classificacao === "inverno"){
+            return {
+                nome: item.nome,
+                preco: item.preco,
+                classificacao: item.classificacao,
+                precoDesconto:(item.preco - (item.preco * 10)/100)
+            } 
+        }else if (item.classificacao === "banho"){
+            return {
+                nome: item.nome,
+                preco: item.preco,
+                classificacao: item.classificacao,
+                precoDesconto:(item.preco - (item.preco*4)/100)
+            } 
+        }else if (item.classificacao === "intimas"){
+            return {
+                nome: item.nome,
+                preco: item.preco,
+                classificacao: item.classificacao,
+                precoDesconto:(item.preco - (item.preco*7)/100)
+            } 
+        }
+    })
 
-    if(classificacao === "verao"){
-        return (preco - (preco * 5)/100)
-    }else if (classificacao === "inverno"){
-        return (preco - (preco * 10)/100)
-    }else if (classificacao === "banho"){
-        return (preco - (preco*4)/100)
-    }else if (classificacao === "intimas"){
-        return (preco - (preco*7)/100)
-    }
 }
 
+const array = [
+    {
+        nome:"Camiseta",
+        preco:40,
+        classificacao: "banho"
+    }
+]
 
-
-console.log(procurarProduto("Camiseta", 40, "banho"))
-
-//////////INCOMPLETO
+console.log(procurarProduto(array))
