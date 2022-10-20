75704248dcfaf86ed54c67d707b09cbecab5b2de
Bruna de Carvalho <brunacarvalho260697@gmail.com>
2022-10-20 15:32:57 -0300

    FIX: Alteração do any no exercicio 6

---
+++ modulo5/lista-typescript/src/exercicio3.ts
@@ -0,0 +1,13 @@
+enum GENERO {
+	ACAO="ação",
+	DRAMA="drama",
+	COMEDIA="comédia",
+	ROMANCE="romance",
+	TERROR="terror"
+}
+
+type filme ={
+    nome:string,
+    ano:number, 
+    genero:GENERO,
+}
