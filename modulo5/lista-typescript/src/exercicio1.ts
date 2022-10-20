d914a03403841fb0800ecb2bb2ff6a072b359e19
Bruna de Carvalho <brunacarvalho260697@gmail.com>
2022-10-20 20:26:18 -0300

    FIX: Melhoria no exercicio 1

--- modulo5/lista-typescript/src/exercicio1.ts
+++ modulo5/lista-typescript/src/exercicio1.ts
@@ -1,25 +1,8 @@
-function nascimento(nome:string, nascimento:string){
+function nascimento(nome:string, nascimento:string):string{
     let seuNome = nome
-    let transformarEmArray = nascimento.split("")
-    let dia = []
-    dia.push(transformarEmArray[0])
-    dia.push(transformarEmArray[1])
-    let juntarDia = dia.join("")
+    let transformarEmArray = nascimento.split("/")
 
-    let mes = []
-    mes.push(transformarEmArray[3])
-    mes.push(transformarEmArray[4])
-    let juntarMes = mes.join("")
-
-
-    let ano = []
-    ano.push(transformarEmArray[6])
-    ano.push(transformarEmArray[7])
-    ano.push(transformarEmArray[8])
-    ano.push(transformarEmArray[9])
-    let juntarAno = ano.join("")
-
-    return (`Olá, me chamo ${seuNome}, nasci no dia ${juntarDia} do mês ${juntarMes} do ano de ${juntarAno}`)
+    return (`Olá, me chamo ${seuNome}, nasci no dia ${transformarEmArray[0]} do mês ${transformarEmArray[1]} do ano de ${transformarEmArray[2]}`)
 }
 
-console.log(nascimento("Bruna", ("26/06/1997")))
+console.log(nascimento("Bruna", "26/06/1997"))
