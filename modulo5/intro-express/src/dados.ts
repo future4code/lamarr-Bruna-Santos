96f7a9ba2897c47004ef0d199beb684276a87f32
Bruna de Carvalho <brunacarvalho260697@gmail.com>
2022-10-25 22:52:19 -0300

    FEAT: Resolução dos exercicios: 2,3,4,5,6 e 7

--- modulo5/intro-express/src/dados.ts
+++ modulo5/intro-express/src/dados.ts
@@ -1,9 +1,49 @@
-export const usuarios = [
+//exercicio 2:
+
+type Usuario ={
+    id:number
+    name:string
+    phone: number
+    email:string 
+    website:string
+}
+
+// exercicio 3:
+export const usuarios:Usuario[] = [
     {
-        id: "",
-        nome: "",
-        telefone: "",
-        email:"",
-        website:""
-    }
+        id: 1,
+        name: "Alicent",
+        phone: 2345-8986,
+        email: "alicent@lamarr.com.br",
+        website: "alicent.com.br"
+    },
+    {
+        id: 2,
+        name: "Aemond",
+        phone: 3245-3962,
+        email: "aemond@lamarr.com.br",
+        website: "aemond.com.br"
+    },
+    {
+        id: 3,
+        name: "Viserys",
+        phone: 2335-5690,
+        email: "viserys@lamarr.com.br",
+        website: "viserys.com.br"
+    },
+    {
+        id: 4,
+        name: "Rhaenyra",
+        phone: 2455-8469,
+        email: "rhaenyra@lamarr.com.br",
+        website: "rhaenyra.com.br"
+    },
+    {
+        id: 5,
+        name: "Daemon",
+        phone: 2345-8986,
+        email: "daemon@lamarr.com.br",
+        website: "daemon.com.br"
+    },
 ]
+
--- modulo5/intro-express/src/exercicio1.ts
+++ modulo5/intro-express/src/exercicio1.ts
@@ -1,5 +1,7 @@
 import express, {Request, Response} from 'express'
 import cors from 'cors'
+import { usuarios } from './dados'
+import { posts } from './posts'
 
 const app = express() 
 
@@ -7,6 +9,7 @@ app.use(express.json())
 
 app.use(cors())
 
+// exercicio 1:
 app.get("/",(req:Request, res:Response)=>{
     const id = req.headers.id
     console.log(id)
@@ -17,14 +20,33 @@ app.listen(3003, () => {
     console.log("Servidor executando na porta 3003");
 })
 
-//exercicio 2:
+// exercicio 4:
+app.get("/usuarios", (req:Request, res:Response)=> {
+    const usuarioMock = usuarios.map((user)=>{
+        return user
+    })
+    res.status(200).send(usuarioMock)
+})
 
-type Usuario ={
-    id:number
-    name:string
-    phone: number
-    email:string 
-    website:string
-}
+// exercicio 7:
+app.get("/posts", (req:Request, res:Response)=> {
+    const postagensMock = posts.map((post)=>{
+        return post
+    })
+    res.status(200).send(postagensMock)
+})
 
-// exercicio 3:
+// exercicio 8:
+app.get("/posts/usuario",(req:Request, res:Response)=>{
+    const idUsuario = req.headers.idUser
+
+    if(!idUsuario){
+        res.status(400).send("Colocar um id válido")
+    }
+
+    const procurarUsuario = posts.find((user)=>{
+        return user.id === idUsuario
+    })
+
+    return res.status(200).send(procurarUsuario)
+})
