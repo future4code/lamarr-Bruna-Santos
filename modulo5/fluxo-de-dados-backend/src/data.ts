075653da35435fe2fe98966f54edf58c99c3e6d7
Bruna de Carvalho <brunacarvalho260697@gmail.com>
2022-11-03 16:36:04 -0300

    FEAT: Resolução do exercicio 2

---
+++ modulo5/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,28 @@
+type Mercado={
+    id:string
+    name: string
+    price: number
+}
+
+export const mercado:Mercado[] = [
+    {
+        id: "B01",
+        name: "Bolacha Trakinas",
+        price: 3.40
+    },
+    {
+        id: "I01",
+        name: "Iogurte Nestlé",
+        price: 2.90
+    },
+    {
+        id: "F01",
+        name: "Manga",
+        price: 4.90
+    },
+    {
+        id: "B01",
+        name: "Bolacha Passatempo",
+        price: 3.50
+    },
+]
