bf5d99259c84986c419922617fe7bae3d9daa4aa
Bruna de Carvalho <brunacarvalho260697@gmail.com>
2022-11-04 21:52:44 -0300

    FEAT: Criação dos types e ENUM no arquivo Dados e resolução da letra A do exericicio 1

---
+++ modulo5/apis-rest/src/index.ts
@@ -0,0 +1,18 @@
+import express from "express"
+
+import cors from 'cors'
+
+const app = express()
+
+app.use(express.json())
+
+app.use(cors())
+
+
+// Exercicio 1:
+// Método GET.
+
+
+app.listen(3003, () => {
+    console.log("Server is running in http://localhost:3003");
+});
