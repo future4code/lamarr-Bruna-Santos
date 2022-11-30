29ee171804e7374b023fb0dbc3f1fd7195a3417b
Bruna de Carvalho <brunacarvalho260697@gmail.com>
2022-11-29 22:18:02 -0300

    FEAT: Resolução do exercicio 3

--- modulo5/servicos-backend/src/index.ts
+++ modulo5/servicos-backend/src/index.ts
@@ -1,4 +1,6 @@
 import { app } from "./app";
 import { address } from "./endpints/address";
+import createUser from "./endpints/createUser";
 
 app.post("/address", address)
+app.post("/create", createUser)
